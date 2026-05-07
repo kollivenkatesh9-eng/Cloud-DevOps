da5ad2a90a2ce4a87c5bd96f4d07162f8500ecc8
kollivenkatesh9-eng <kollivenkatesh9@gmail.com>
2026-05-07 10:42:34 +0530

    changes made in app tf file

--- app.tf
+++ app.tf
@@ -7,5 +7,5 @@ variable "region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.micro description 2.0"
 }
